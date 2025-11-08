70769a26d09e320942c8db5d0fcaa806884d31b3
Aditya Jagtap <jagtapaditya001@gmail.com>
2025-11-08 11:32:39 +0530

    Add files via upload
    Changes

---
+++ 7.1_reciever.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <ctype.h>
+#include <string.h>
+
+struct msg_buffer
+{
+    long msg_type;
+    char msg_text[100];
+};
+
+int main()
+{
+    key_t key = ftok("msgfile", 65);
+    int msgid = msgget(key, 0666 | IPC_CREAT);
+
+    struct msg_buffer message;
+
+    msgrcv(msgid, &message, sizeof(message.msg_text), 0, 0);
+
+    for (int i = 0; message.msg_text[i]; i++)
+    {
+        message.msg_text[i] = toupper(message.msg_text[i]);
+    }
+
+    printf("Received Message (Uppercase): %s\n", message.msg_text);
+
+    msgctl(msgid, IPC_RMID, NULL);
+
+    return 0;
+}
