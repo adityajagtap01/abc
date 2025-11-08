b0708a5e26f9092614320ea073777188fc8521ae
Aditya Jagtap <jagtapaditya001@gmail.com>
2025-11-08 11:34:17 +0530

    complete 7.1.c

--- 7.1_reciever.c
+++ 7.1_reciever.c
@@ -1,3 +1,66 @@
+
+
+
+
+
+
+#sender
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+
+struct msg_buffer
+{
+    long msg_type;
+    char msg_text[100];
+};
+
+int main()
+{
+    key_t key = ftok("msgfile", 65);
+    int msgid = msgget(key, 0666 | IPC_CREAT);
+
+    struct msg_buffer message;
+
+    printf("Enter message type (integer): ");
+    scanf("%ld", &message.msg_type);
+    getchar();
+
+    printf("Enter message text: ");
+    fgets(message.msg_text, sizeof(message.msg_text), stdin);
+
+    msgsnd(msgid, &message, sizeof(message.msg_text), 0);
+
+    printf("Message sent successfully.\n");
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#reciever
+
+
+
+
+
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ipc.h>
@@ -30,4 +93,5 @@ int main()
     msgctl(msgid, IPC_RMID, NULL);
 
     return 0;
+
 }
